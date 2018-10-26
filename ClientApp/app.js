ff8a8a8f7d4815e867e7758f0cb6a23e1ba25a4f
Guillaume <shatteredshadows30@gmail.com>
2018-10-26 13:48:30 -0400

    Initial commit

---
+++ ClientApp/app.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import axios from 'axios'
+import router from './router/index'
+import store from './store'
+import { sync } from 'vuex-router-sync'
+import App from 'components/app-root'
+import { FontAwesomeIcon } from './icons'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_API_TOKEN',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then disable the following:
+  // installComponents: true,
+})
+
+// Registration of global components
+Vue.component('icon', FontAwesomeIcon)
+
+Vue.prototype.$http = axios
+
+sync(store, router)
+
+const app = new Vue({
+  store,
+  router,
+  ...App
+})
+
+export {
+  app,
+  router,
+  store
+}
